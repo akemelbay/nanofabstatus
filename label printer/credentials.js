e5568015f327b95a04601bb5fdc725720f61007b
akemelbay <a.kemelbay@gmail.com>
2025-04-13 14:44:17 -0700

    Delete label printer directory

--- label printer/credentials.js
+++
@@ -1,8 +0,0 @@
-// Your Google Sheets API key
-const API_KEY = 'AIzaSyDijBK0S7hJ7NdszRi3i1onzM0z0COJMq8'; // Replace with your actual API key
-
-// The ID of the Google Spreadsheet
-const SPREADSHEET_ID = '1zow6QaSb-HTmyIvV8M2lb_g6S3VSlcOLommhOY8p0Oo'; // Replace with your actual spreadsheet ID
-
-// The name of the worksheet
-const SHEET_NAME = 'Sputter';
